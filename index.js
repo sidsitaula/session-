0d555a3185c361c6b9f6e8dd21798867b17ca0f5
sidsitaula <siddharthasitaula02@gmail.com>
2022-11-01 13:47:09 +0545

    session maxAge

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express=require('express')
-const redis=require('redis')
 var app = express()
 var session = require('express-session')
 const sqlite3 = require('sqlite3').verbose();
-
+var flash = require('connect-flash');
 var SQLiteStore = require('connect-sqlite3')(session);
 
 
@@ -21,7 +20,11 @@ app.use(session({
   store:new SQLiteStore({
     db: 'sessions.db', dir: './db'
   }),
+  cookie:{
+    maxAge:25000
+  }
   }))
+  app.use(flash());
 
 // Access the session as req.session
 app.get('/', function(req, res, next) {
@@ -38,8 +41,30 @@ app.get('/', function(req, res, next) {
     });
     console.log('---end---')
   });
-  req.session.cookie.maxAge=5000
+
   return res.json({msg:req.session.msg})
+})
+
+app.get('/login', function(req, res, next) {
+  req.session.isAuth=true;
+  req.session.msg="GOOD";
+  req.session.cookie.maxAge=25000
+  return res.json({msg:'logged in'})
+})
+
+app.get('/secret', function(req,res,next){
+  if(!req.session.isAuth){
+    req.flash('err','not authenticated');
+    return res.json({err:req.flash('err')});
+  }
+  console.log(req.flash('info'))
+  return res.json({msg:"welcome"})
+})
+
+app.get('/logout', function(req,res,next){
+  req.session.destroy();
+  res.clearCookie('sidgg')
+  return res.send("Logged out")
 })
 
 app.listen(3000)
